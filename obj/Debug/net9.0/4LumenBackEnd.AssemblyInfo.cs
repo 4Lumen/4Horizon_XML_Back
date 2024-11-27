c7b6778fa5b06c144f4d3129e3d861b50828ac56
gmunaro <gmunaro@gmail.com>
2024-11-27 14:57:30 -0300

    Tentativa 4 .yaml

--- obj/Debug/net9.0/4LumenBackEnd.AssemblyInfo.cs
+++ obj/Debug/net9.0/4LumenBackEnd.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4LumenBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126dca853e69cbe28690463fb61a49933306b0de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42bf0fb28b74cbf88352fabaa103d1780b82be36")]
 [assembly: System.Reflection.AssemblyProductAttribute("4LumenBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4LumenBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
